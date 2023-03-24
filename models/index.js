0ac55e17f0a6721d28dbc93daec68e13ba4ceef1
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2023-03-24 15:23:10 -0400

    initial commit with some DB's

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const parkingLot = require('./parkingLot');
+const parkingSpace = require('./parkingSpace');
+
+
+module.exports = { User, parkingLot, parkingSpace }
