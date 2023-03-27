f4b20b290267035d5bf8122a02d59f483fe9dc70
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2023-03-27 19:03:14 -0400

    Car information form
    Added a form for the vehicle information that will be added by the user.

---
+++ parking-manager/src/components/userCarForm.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import {license_plate, make, model, color} from ""
+
+function Form() {
+  // Here we set four state variables for carmodel, carmake,carlicense, and carcolor using `useState`
+  const [model, setCarModel] = useState('');
+  const [make, setCarMake] = useState('');
+  const [license_plate, setCarLicensePlate] = useState('');
+  const [color, setCarColor] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { model, value } = e.target;
+
+    // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
+    // return name === 'firstName' ? setFirstName(value) : setLastName(value);
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // Alert the user their first and last name, clear the inputs
+    alert(`Hello ${firstName} ${lastName}`);
+    setCarModel('');
+    setCarMake('');
+    setcarLicense('');
+    setCarColor('');
+  };
+
+  return (
+    <div>
+      <p>
+        Hello {firstName} {lastName}
+      </p>
+      <form className="form">
+        <input
+          value={carModel}
+          name="carModel"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Car Model"
+        />
+        <input
+          value={carMake}
+          name="carMake"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Car Make"
+        />
+         <input
+          value={carLicense}
+          name="carLicense"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Car License"
+        />
+        <input
+          value={carColor}
+          name="carColor"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Car Color"
+        />
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
