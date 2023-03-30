00586806fe5dc4f1e90d542cb6cf619db79ef861
unknown <lexiarbajianclt@gmail.com>
2023-03-30 19:49:00 -0400

    commit to pull

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,11 +4,12 @@ const typeDefs = gql `
 type User {
     _id: ID!
     password:String!
-    name:String!
+    name:String
     username:String!
-    phoneNumber:String!
+    phoneNumber:String
     cars: [Car]
     paid: Boolean
+    email:String
 }
 
 type Car {
@@ -43,23 +44,25 @@ type Query {
     user(username:String!):User
     me:User
     cars(username:String!):[Car]
-    car(carId:ID!):Car
+    car(license_plate:String!):Car
     lot(lotName:String!):Lot
 } 
 
 type Mutation {
     createUser(username: String!, email: String!, password: String!, phoneNumber:String!, name:String!): Auth
     login(email: String!, password: String!): Auth
-    updateUser(userId: ID!): Auth
-   
+    updateUser(userId:ID!): Auth
     deleteUser(userId:ID!): Auth
+
     addUserCar(license_plate:String!): Car
-    deleteUserCar(license_plate:String!, carId:ID!): Car
-    updateUserCar(carId:ID!): Car
-    addCarSpace(carId:ID!, spaceName:String!): Car
-    deleteCarSpace(spaceId:ID!, carId:ID!): Car
-    occupySpace(spaceId:ID!, lotId: ID!): Space
-    vacateSpace(spaceId:ID!, lotId:ID!):Space
+    deleteUserCar(license_plate:String!): Car
+    updateUserCar(license_plate:String!): Car
+
+    addCarSpace(license_plate:String!, spaceName:String!): Car
+    deleteCarSpace(spaceName:String!, license_plate:String!): Car
+
+    occupySpace(spaceName:String!, lotName:String!): Space
+    vacateSpace(spaceName:String!, lotName:String!):Space
   }
 `;
 
