ae3e5601127596be1d929830111415e3a7a53ecd
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2023-04-03 16:23:56 -0400

    added code for members list
    Added necessary code for members list to iterate through all user on present their information

--- client/src/components/membershipList.js
+++ client/src/components/membershipList.js
@@ -1,66 +1,78 @@
 import React from 'react';
+import { QUERY_USERS } from "../utils/queries"
+import { useQuery } from "@apollo/client"
 
-export default function List({ users }) {
-  return (
-   <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    Client Name
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Car Make/Model (Color)
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Phone Number
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Email
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Payment Status
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Alex Luborev
-                </th>
-                <td class="px-6 py-4">
-                    Bugatti
-                </td>
-                <td class="px-6 py-4">
-                   555-555-5555
-                </td>
-                <td class="px-6 py-4">
-                    Alexdaddy555@hut.com
-                </td>
-                <td class="px-6 py-4 text-green-500">
-                    PAID
-                </td>
-            </tr>
-            <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Yiorgos Bosnakis
-                </th>
-                <td class="px-6 py-4">
-                    Nissan Altima (Silver)
-                </td>
-                <td class="px-6 py-4">
-                    555-657-1192
-                </td>
-                <td class="px-6 py-4 ">
-                    YiorgyBear@aol.com
-                </td>
-                <td class="px-6 py-4 text-orange-700">
-                    NOT PAID
-                </td>
-            </tr>
-            </tr>
-        </tbody>
-    </table>
-</div>
-  );
+export default function MembersList() {
+    const { loading, data } = useQuery(QUERY_USERS);
+
+    const userList = data?.users || [];
+
+    return (
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            {loading ? (
+                <div>Loading...</div>
+            ) : (
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Client Name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Car Make/Model (Color)
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Phone Number
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Email
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Payment Status
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userList.map((user) => {
+                           return( <tr key={user._id} class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {user.name}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {user.cars[0].make}{user.cars[0].model}{user.cars[0].color}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {user.phoneNumber}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {user.email}
+                                </td>
+                                <td class="px-6 py-4 text-green-500">
+                                    {user.paid}
+                                </td>
+                            </tr>)
+                        })}
+
+                        <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Yiorgos Bosnakis
+                            </th>
+                            <td class="px-6 py-4">
+                                Nissan Altima (Silver)
+                            </td>
+                            <td class="px-6 py-4">
+                                555-657-1192
+                            </td>
+                            <td class="px-6 py-4 ">
+                                YiorgyBear@aol.com
+                            </td>
+                            <td class="px-6 py-4 text-orange-700">
+                                NOT PAID
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            )}
+        </div>
+    );
 }
