8036b42ae794ba56857522f41a16ca22bf06e324
Ethan Quinlan <ethan.quinlan2016@gmail.com>
2023-04-03 20:26:38 -0400

    updated car form style

--- client/src/components/Forms/userCarForm.js
+++ client/src/components/Forms/userCarForm.js
@@ -60,91 +60,88 @@ function CarForm() {
   };
 
   return (
-    <div>
-      {Auth.loggedIn() ? (
+    <>
+      <main className="bg-gray-50 dark:bg-gray-900 p-6  ">
         <form
           onSubmit={handleFormSubmit}
           action="#"
-          className=" max-w-lg flex flex-col mx-auto md:h-screen lg:py-0 p-6"
+          className="p-6 max-w-lg flex flex-col  mx-auto md:h-screen lg:py-0"
         >
-          <h2 className="p-6 text-white">Vehicle Information</h2>
-          <div className=" border p-6">
-            <div className="w-full md:w-1/2 px-4 mb-2 md:mb-0">
-              <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2">
-                Car Model
-              </label>
-              <input
-                value={formState.model}
-                name="carModel"
-                onChange={handleChange}
-                type="text"
-                placeholder="Car Model"
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              />
-            </div>
-          </div>
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-3">
-              Car Make
-            </label>
-            <input
-              value={formState.make}
-              name="carMake"
-              onChange={handleChange}
-              type="text"
-              placeholder="Car Make"
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            />
-          </div>
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-3">
-              Car Color
-            </label>
+          <div className="bg-gray-200  ">
+            <h2 className="p-6 text-center">Vehicle Information</h2>
+            <div className=" border p-6">
+              <div className="">
+                <div className="w-full md:w-1/2 px-4 mb-2 md:mb-0">
+                  <label className="block uppercase tracking-wide  text-xs font-bold mb-2">
+                    Car Model
+                  </label>
+                  <input
+                    value={formState.model}
+                    name="model"
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="Car Model"
+                    className="appearance-none block w-full bg-black text-gray-700 border border-gray-900 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                    required=""
+                  />
+                </div>
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-3">
+                  Car Make
+                </label>
+                <input
+                  value={formState.make}
+                  name="make"
+                  onChange={handleChange}
+                  type="text"
+                  placeholder="Car Make"
+                  className="appearance-none block w-full bg-black text-gray-700 border border-gray-900 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  required=""
+                />
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-3">
+                  Car Color
+                </label>
 
-            <input
-              value={formState.color}
-              name="carColor"
-              onChange={handleChange}
-              type="text"
-              placeholder="Car Color"
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            />
-          </div>
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-3">
-              License Number
-            </label>
-            <input
-              value={formState.license_plate}
-              name="carLicense"
-              onChange={handleChange}
-              type="text"
-              placeholder="Car License"
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            />
-          </div>
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0 p-6">
-            <button
-              type="submit"
-              placeholder="Car License"
-              className="appearance-none block w-full  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            >
-              Submit
-            </button>
-          </div>
-          {error && (
-            <div className="col-12 my-3 bg-danger text-white p-3">
-              {error.message}
+                <input
+                  value={formState.color}
+                  name="color"
+                  onChange={handleChange}
+                  type="text"
+                  placeholder="Car Color"
+                  className="appearance-none block w-full bg-black text-gray-700 border border-gray-900 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  required=""
+                />
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-3">
+                  License Number
+                </label>
+                <input
+                  value={formState.license_plate}
+                  name="license_plate"
+                  onChange={handleChange}
+                  type="text"
+                  placeholder="Car License"
+                  required=""
+                  className="appearance-none block w-full bg-black text-gray-700 border border-gray-900 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                />
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0 ">
+                <button
+                  type="submit"
+                  className="appearance-none block w-full text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                >
+                  Submit
+                </button>
+              </div>
             </div>
-          )}
+          </div>
         </form>
-      ) : (
-        <p>
-          You need to be logged in to share your thoughts. Please{" "}
-          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
-        </p>
-      )}
-    </div>
+      </main>
+    </>
   );
 }
 
