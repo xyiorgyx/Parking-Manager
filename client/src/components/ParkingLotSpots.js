c3c6726564523a5a44babfa45292211cd7f993b8
Alex Loburev <48661813+Beaterok@users.noreply.github.com>
2023-04-04 17:39:21 -0400

    added spaces loading

--- client/src/components/ParkingLotSpots.js
+++ client/src/components/ParkingLotSpots.js
@@ -1,9 +1,21 @@
-import React from 'react';
-
+import { React } from 'react';
+import { QUERY_SPACE } from '../utils/queries';
+import { useQuery } from "@apollo/client";
 
 export default function ParkingLotSpots() {
+
+    const { loading, data } = useQuery(QUERY_SPACE);
+
+    const spaces = data?.spaces || [];
+
+
+const handleChange(name)=(e) =>{
+ 
+}
+    console.log(spaces)
+
     return (
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="mx-auto flex flex-col   lg:px-40 overflow-x-auto shadow-md sm:rounded-lg">
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
@@ -14,203 +26,33 @@ export default function ParkingLotSpots() {
                             Parking Space
                         </th>
                         <th scope="col" className="px-6 py-3">
-                            Floor
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Address
-                        </th>
-                        <th scope="col" className="px-6 py-3">
                             Reservation
                         </th>
                     </tr>
                 </thead>
+                {loading ? (
+        <div>Loading...</div>
+      ) : (
+        spaces.map((space) => {
+          return (
                 <tbody>
                     <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            7th Street Station
+                            Parking Lot: {space.parkingLot}
                         </th>
                         <td className="px-6 py-4">
-                           332
-                        </td>
-                        <td className="px-6 py-4">
-                            3rd Floor
+                            Parking Space: {space.spaceName}
                         </td>
                         <td className="px-6 py-4 ">
-                            219 E 7th StFirst Ward Charlotte, NC 28202
+                            Occupied: {space.occupied}
                         </td>
                         <td className="px-6 py-4 text-green-500">
-                            <button>Reserve</button>
+                            <button onClick = {handleChange(space.spaceName)}>Reserve</button>
                         </td>
                     </tr>
                 </tbody>
-                <tbody>
-                    <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            7th Street Station
-                        </th>
-                        <td className="px-6 py-4">
-                            442
-                        </td>
-                        <td className="px-6 py-4">
-                            4th Floor
-                        </td>
-                        <td className="px-6 py-4 ">
-                            219 E 7th StFirst Ward Charlotte, NC 28202
-                        </td>
-                        <td className="px-6 py-4 text-green-500">
-                            <button>Reserve</button>
-                        </td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            7th Street Station
-                        </th>
-                        <td className="px-6 py-4">
-                            459
-                        </td>
-                        <td className="px-6 py-4">
-                            4th Floor
-                        </td>
-                        <td className="px-6 py-4 ">
-                            219 E 7th StFirst Ward Charlotte, NC 28202
-                        </td>
-                        <td className="px-6 py-4 text-green-500">
-                            <button>Reserve</button>
-                        </td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            7th Street Station
-                        </th>
-                        <td className="px-6 py-4">
-                            412
-                        </td>
-                        <td className="px-6 py-4">
-                            4th Floor
-                        </td>
-                        <td className="px-6 py-4 ">
-                            219 E 7th StFirst Ward Charlotte, NC 28202
-                        </td>
-                        <td className="px-6 py-4 text-green-500">
-                            <button>Reserve</button>
-                        </td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            7th Street Station
-                        </th>
-                        <td className="px-6 py-4">
-                            546
-                        </td>
-                        <td className="px-6 py-4">
-                            5th Floor
-                        </td>
-                        <td className="px-6 py-4 ">
-                            219 E 7th StFirst Ward Charlotte, NC 28202
-                        </td>
-                        <td className="px-6 py-4 text-green-500">
-                            <button>Reserve</button>
-                        </td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            7th Street Station
-                        </th>
-                        <td className="px-6 py-4">
-                            570
-                        </td>
-                        <td className="px-6 py-4">
-                            5th Floor
-                        </td>
-                        <td className="px-6 py-4 ">
-                            219 E 7th StFirst Ward Charlotte, NC 28202
-                        </td>
-                        <td className="px-6 py-4 text-green-500">
-                            <button>Reserve</button>
-                        </td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            7th Street Station
-                        </th>
-                        <td className="px-6 py-4">
-                            599
-                        </td>
-                        <td className="px-6 py-4">
-                            5th Floor
-                        </td>
-                        <td className="px-6 py-4 ">
-                            219 E 7th StFirst Ward Charlotte, NC 28202
-                        </td>
-                        <td className="px-6 py-4 text-green-500">
-                            <button>Reserve</button>
-                        </td>
-                    </tr>
-                </tbody>     <tbody>
-                    <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            7th Street Station
-                        </th>
-                        <td className="px-6 py-4">
-                            667
-                        </td>
-                        <td className="px-6 py-4">
-                            6th Floor
-                        </td>
-                        <td className="px-6 py-4 ">
-                            219 E 7th StFirst Ward Charlotte, NC 28202
-                        </td>
-                        <td className="px-6 py-4 text-green-500">
-                            <button>Reserve</button>
-                        </td>
-                    </tr>
-                </tbody>     <tbody>
-                    <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            7th Street Station
-                        </th>
-                        <td className="px-6 py-4">
-                            690
-                        </td>
-                        <td className="px-6 py-4">
-                            6th Floor
-                        </td>
-                        <td className="px-6 py-4 ">
-                            219 E 7th StFirst Ward Charlotte, NC 28202
-                        </td>
-                        <td className="px-6 py-4 text-green-500">
-                            <button>Reserve</button>
-                        </td>
-                    </tr>
-                </tbody>     <tbody>
-                    <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            7th Street Station
-                        </th>
-                        <td className="px-6 py-4">
-                            779
-                        </td>
-                        <td className="px-6 py-4">
-                            7th Floor
-                        </td>
-                        <td className="px-6 py-4 ">
-                            219 E 7th StFirst Ward Charlotte, NC 28202
-                        </td>
-                        <td className="px-6 py-4 text-green-500">
-                            <button>Reserve</button>
-                        </td>
-                    </tr>
-                </tbody>
+             )
+          }))}
             </table>
 
         </div>
