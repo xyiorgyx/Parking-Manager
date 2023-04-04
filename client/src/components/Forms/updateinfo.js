8930cfc49838542355a86f89dc699e5e1258b7c2
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2023-04-03 22:58:32 -0400

    tiny bug where styling had wrong class name

--- client/src/components/Forms/updateinfo.js
+++ client/src/components/Forms/updateinfo.js
@@ -38,7 +38,7 @@ const UpdateUser = () => {
         <main className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div classNameName="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 class="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Update your information
                         </h1>
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -12,7 +12,7 @@ const Signup = () => {
     phoneNumber: "",
     name: "",
   });
-  const [addUser,{ error, data } ] = useMutation(ADD_USER);
+  const [addUser, { error, data }] = useMutation(ADD_USER);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -40,7 +40,7 @@ const Signup = () => {
     <main className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div classNameName="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 class="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Create an account
             </h1>
@@ -136,7 +136,7 @@ const Signup = () => {
                 />
               </div>
               <button
-                
+
                 className="w-full border text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Create an account
