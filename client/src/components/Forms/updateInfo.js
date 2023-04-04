227985291a4bafdbf85cf842e16ab36d97dc7e81
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2023-04-04 08:55:02 -0400

    Update updateInfo.js

--- client/src/components/Forms/updateInfo.js
+++ client/src/components/Forms/updateInfo.js
@@ -1,11 +1,159 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { useStoreContext } from '../utils/GlobalState';
-import { UPDATE_USER } from '../../utils/actions';
-import { QUERY_USER } from '../../utils/queries';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { UPDATE_USER } from "../utils/mutations";
+import Auth from "../../utils/Auth";
 
-function UpdateUser {
+const UpdateUser = () => {
+    const [formState, setFormState] = useState({
+        username: "",
+        email: "",
+        password: "",
+        phoneNumber: "",
+        name: "",
+    });
+    const [updateUser, { error, data }] = useMutation(UPDATE_USER);
 
-    
-}
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const { data } = await updateUser({
+                variables: { ...formState },
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+
+    return (
+        <main className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div classNameName="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 class="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Update your information
+                        </h1>
+                        <form
+                            className="space-y-4 md:space-y-6"
+                            action="#"
+                            onSubmit={handleFormSubmit}
+                        >
+                            <div>
+                                <label
+                                    htmlFor="name"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Your Name
+                                </label>
+                                <input
+                                    className="form-input bg-gray-50 border  border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    type="text"
+                                    name="name"
+                                    value={formState.name}
+                                    onChange={handleChange}
+                                    placeholder="Name"
+                                    required=""
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="name"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Your Phone Number
+                                </label>
+                                <input
+                                    className="form-input bg-gray-50 border  border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    type="text"
+                                    name="phoneNumber"
+                                    value={formState.phoneNumber}
+                                    onChange={handleChange}
+                                    placeholder="Phone Number"
+                                    required=""
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="name"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Your Username
+                                </label>
+                                <input
+                                    className="form-input bg-gray-50 border  border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    type="text"
+                                    name="username"
+                                    value={formState.username}
+                                    onChange={handleChange}
+                                    placeholder="Username"
+                                    required=""
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="email"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Your email
+                                </label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    value={formState.email}
+                                    onChange={handleChange}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="name@company.com"
+                                    required=""
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="password"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    value={formState.password}
+                                    placeholder="••••••••"
+                                    onChange={handleChange}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    required=""
+                                />
+                            </div>
+                            <button
+
+                                className="w-full border text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            >
+                                Update information
+                            </button>
+                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                                Return to login{" "}
+                                <Link
+                                    to="/login"
+                                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                >
+                                    Login here
+                                </Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default UpdateUser;
